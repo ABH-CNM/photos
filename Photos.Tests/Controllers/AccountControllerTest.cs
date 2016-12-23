932f45f7fe2a9ea4112526f15ae5a59ba31755b3
Kim Huy <kitechuz@gmail.com>
2016-12-23 16:58:11 +0700

    initial

---
+++ Photos.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Photos.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photos.Tests.Controllers
+{
+    [TestClass]
+    class AccountControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+        }
+    }
+}
---
+++ Photos/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Photos.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
